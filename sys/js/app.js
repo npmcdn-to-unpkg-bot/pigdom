82d1a8b8422a6ea129aeef1a7e66f4c238c4a597
Victor H. Avelar <aovo128446@upemor.edu.mx>
2016-09-01 13:15:55 -0500

    Observaciones versión 2

--- sys/js/app.js
+++ sys/js/app.js
@@ -2,12 +2,12 @@ new Vue({
   el: '#checklist',
   data: {
     todos: [
-      {title: "Carta de Responsabilidad- líder INEEL", link: "convocatorias/fish/anexo1.doc", completed: false},
-      {title: "Cartas de intención- líder INEEL", link: "convocatorias/fish/anexo2.doc", completed: false},
-      {title: "Cartas de intención- líder IMP", link: "convocatorias/fish/anexo3.doc", completed: false},
-      {title: "Cartas de intención en Inglés - líder IMP ", link: "convocatorias/fish/anexo4.doc", completed: false},
-      {title: "Plan Estratégico de Fortalecimiento Institucional", link: "convocatorias/fish/anexo5.doc", completed: false},
-      {title: "Plan General del Proyecto", link: "convocatorias/fish/anexo6.doc", completed: false},
+      {title: "Carta de Responsabilidad- líder INEEL", link: "convocatorias/fish/anexo1.doc", completed: false, type: "edit"},
+      {title: "Cartas de intención- líder INEEL", link: "convocatorias/fish/anexo2.doc", completed: false, type: "edit"},
+      {title: "Cartas de intención- líder IMP", link: "convocatorias/fish/anexo3.doc", completed: false, type: "edit"},
+      {title: "Cartas de intención en Inglés - líder IMP ", link: "convocatorias/fish/anexo4.doc", completed: false, type: "edit"},
+      {title: "Plan Estratégico de Fortalecimiento Institucional", link: "convocatorias/fish/anexo5.docx", completed: false, type: "view"},
+      {title: "Plan General del Proyecto", link: "convocatorias/fish/anexo6.docx", completed: false, type: "view"},
     ]
   },
   methods: {
