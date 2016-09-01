0ecceb3cd031e28d66569c7c9d03f8daa1ee484a
Victor H. Avelar <aovo128446@upemor.edu.mx>
2016-09-01 14:22:46 -0500

    Cambios de estilo

--- sys/js/app.js
+++ sys/js/app.js
@@ -4,8 +4,8 @@ new Vue({
     todos: [
       {title: "Carta de Responsabilidad- líder INEEL", link: "convocatorias/fish/anexo1.doc", completed: false, type: "edit"},
       {title: "Cartas de intención- líder INEEL", link: "convocatorias/fish/anexo2.doc", completed: false, type: "edit"},
-      {title: "Cartas de intención- líder IMP", link: "convocatorias/fish/anexo3.doc", completed: false, type: "edit"},
-      {title: "Cartas de intención en Inglés - líder IMP ", link: "convocatorias/fish/anexo4.doc", completed: false, type: "edit"},
+//      {title: "Cartas de intención- líder IMP", link: "convocatorias/fish/anexo3.doc", completed: false, type: "edit"},
+//      {title: "Cartas de intención en Inglés", link: "convocatorias/fish/anexo4.doc", completed: false, type: "edit"},
       {title: "Plan Estratégico de Fortalecimiento Institucional", link: "convocatorias/fish/anexo5.docx", completed: false, type: "view"},
       {title: "Plan General del Proyecto", link: "convocatorias/fish/anexo6.docx", completed: false, type: "view"},
     ]
